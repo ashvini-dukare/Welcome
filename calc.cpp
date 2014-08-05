e20ac484ab3503489bb102f85eb2a9e26735d314
ashvini-dukare <ashvini.dukare@cumminscollege.in>
2014-08-05 21:59:57 +0530

    Create calc.cpp
    welcome in github

---
+++ calc.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class cal
+{
+  int a,b,c;
+  publiv:
+          void add()
+          {
+            cout<<"\n Enter valus of a & b";
+            c=a+b;
+            cout<<c;
+          }
+          
+};
+int main()
+{
+  cal c;
+  c.add();
+  return 0;
+}
